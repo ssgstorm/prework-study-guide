e4ad8ceacf52bbb1e425dba466423b0fa4f8c36d
unknown <ssgstorm@gmail.com>
2024-05-05 16:56:57 -0400

    completed tasks of js and for loops

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+
+for(let x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+   }
+
+/*
+
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");a
+} else {
+  console.log('Please try again!');
+}
+*/
+
+
